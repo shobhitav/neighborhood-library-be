21343fafc87c73f7e6838c1503bb7e6d965c37aa
shenry45 <shawn45henry@gmail.com>
2019-11-05 21:28:06 -0700

    fixes env vars in knexfiles.js and removes unlinked files

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,5 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
-const keys = require('../config/keys.js');
 
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
