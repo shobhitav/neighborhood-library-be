010cc610350ded88492391918c3d45b7948675e9
Ali Rajaii <rajaii_a@yahoo.com>
2019-11-16 12:53:57 -0500

    required in db on line 3 as to wire in postgres; the full db auth functionality will not work without this, now registration and login are working as expected

--- services/passport.js
+++ services/passport.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
+const db = require('../database/dbConfig.js');
 
 //takes the user from the done call in the passport.use callback, and sets the session to rember them  by the second parameter in done
 //passport stores the user[0].user_credential on req.passport
