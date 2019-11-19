b17bb6720a99ff1feec1c29ddb36e03762381c77
shenry45 <shawn45henry@gmail.com>
2019-11-18 19:50:21 -0700

    Revert "making a createStore"
    This reverts commit 576be99f2fb7bd5ccc4e751dd7c77b16c4cd8229.
    
    Undoing FE work in BE repo

--- borrower/borrowerpage/src/index.js
+++ borrower/borrowerpage/src/index.js
@@ -2,16 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import rootReducer from './reducers';
+import * as serviceWorker from './serviceWorker';
 
-const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>, 
-    document.getElementById('root'));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
