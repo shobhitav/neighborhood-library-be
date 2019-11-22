b00363aace776e42a158ab35fb76aa52e52434d6
Shobhita Vaishnav <to.shobhita@gmail.com>
2019-11-21 17:12:26 -0800

    transaction model and router

--- lender/lenderCollection-model.js
+++ lender/lenderCollection-model.js
@@ -5,37 +5,42 @@ const db=require('../database/dbConfig.js');
     findBookById,
     addBook,
     toggleAvailability,
-    removeBook
+    removeBook,
+    findBookByLenderIdAndGoogleBookId
 }
 
 // Find books by lender Id.
-function findBooksByLenderId(lender_id){
-    return db('lender_collection')
-    .where({lender_id});
+async function findBooksByLenderId(lenderId){
+    return db('lender_collection').where({lender_id: lenderId});
 }
 
 // Find book by lender collection Id (primary key)
-function findBookById(id){
-    return db('lender_collection')
-    .where({id});
+async function findBookById(id){
+    return db('lender_collection').where({id});
 }
 
 // Create a book in lender collection
-async function addBook(lenderBook){
-    await db('lender_collection').insert(lenderBook);
-    return findBooksByLenderId(lenderBook.lender_id);
+async function addBook(lenderCollectionItem){
+    await db('lender_collection').insert(lenderCollectionItem);
+    return findBooksByLenderId(lenderCollectionItem.lender_id);
 }
 
 // Toggle the availability flag
-async function toggleAvailability(lenderBook){
-    const changes = lenderBook;
+async function toggleAvailability(lenderCollectionItem){
+    const changes = lenderCollectionItem;
     changes.is_available = !changes.is_available;
     await db('lender_collection').where({ id: changes.id }).update(changes);
     return findBooksByLenderId(changes.lender_id);
 }
 
 // Remove a book from the lender collection
-async function removeBook(lenderBook){
-    await db('lender_collection').where({ id: lenderBook.id }).del();
-    return findBooksByLenderId(lenderBook.lender_id);
+async function removeBook(lenderCollectionItem){
+    await db('lender_collection').where({ id: lenderCollectionItem.id }).del();
+    return findBooksByLenderId(lenderCollectionItem.lender_id);
 }
+
+// Find book by lender Id and google book id
+async function findBookByLenderIdAndGoogleBookId(lenderId, googleBookId, isAvailable){
+    return db('lender_collection').where({lender_id: lenderId, google_book_id: googleBookId, is_available: isAvailable});
+}
+
--- transaction/transaction-model.js
+++ transaction/transaction-model.js
@@ -1,15 +1,24 @@
 const db=require('../database/dbConfig.js');
  
  module.exports={
-    
+    findTransactionById,
     addTransaction,
-    
+    updateReturnTime
 }
 
-
+// Find transaction for a given id
+async function findTransactionById(id) {
+    return db('transactions').where({id});
+}
 
 // Add a transaction history
-async function addTransaction(transaction){
-    await db('transactions').insert(transaction);
-    // return findTransactions(transaction.lender_id);
+async function addTransaction(transaction) {
+    const [id] = await db('transactions').returning('id').insert(transaction);
+    return findTransactionById(id);
+}
+
+// Update time stamp of return time
+async function updateReturnTime(id) {
+    await db('transactions').where({id}).update({return_time: db.fn.now()});
+    return findTransactionById(id);
 }
