5f50348b0897ac035ba2d1a9d48e038714760f69
shenry45 <shawn45henry@gmail.com>
2019-12-04 20:18:09 -0700

    lender tests PUT and DELETE

--- __tests__/lender.spec.js
+++ __tests__/lender.spec.js
@@ -6,6 +6,7 @@ const db = require('../database/dbConfig.js'); // DB
 const baseURI = '/api/lender-collection';
 
 describe('lender routes', () => {
+  let currBookID = 0;
 
   // clear db before tests
   beforeAll(async () => {
@@ -20,10 +21,11 @@ describe('lender routes', () => {
         .send({
           google_book_id: 1591984,
           lender_id: 1,
-          isbn: 626558,
-          is_available: true
+          isbn: 6265581337
         })
         .then(res => {
+          currBookID = res.body[0].id;
+
           expect(res.status).toBe(201);
           expect(res.body).toHaveLength(1);
         });
@@ -48,26 +50,54 @@ describe('lender routes', () => {
         .get(`${baseURI}/1`)
         .then(res => {
           expect(res.status).toBe(200);
-          // expect(res.body).toHaveLength(1);
-        })
+          expect(res.body).toHaveLength(1);
+        });
     });
 
-    it('', async () => {
-      await request(server);
+    it('return no books', async () => {
+      await request(server)
+        .get(`${baseURI}/00`)
+        .then(res => {
+          expect(res.body).toHaveLength(0);
+        });
     });
   });
 
   // PUT
   describe('update lender book status', () => {
-    it('', async () => {
-      await request(server);
+    it('change is_available to true', async () => {
+      await request(server)
+        .put(`${baseURI}/${currBookID}`)
+        .then(res => {
+          expect(res.body[0].is_available).toBe(true);
+        });
+    });
+
+    it('book not available', async () => {
+      await request(server)
+        .put(`${baseURI}/${currBookID + 2}`)
+        .then(res => {
+          expect(res.status).toBe(404);
+        });
     });
   });
 
   // DELETE
   describe('delete lender book', () => {
-    it('', async () => {
-      await request(server);
+    it('deletes book', async () => {
+      await request(server)
+        .delete(`${baseURI}/${currBookID}`)
+        .then(res => {
+          expect(res.status).toBe(200);
+        });
+    });
+
+    it('book not available', async () => {
+      await request(server)
+        .delete(`${baseURI}/${currBookID + 2}`)
+        .then(res => {
+          expect(res.status).toBe(404);
+        });
     });
   });
 })
